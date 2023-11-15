7259f33332727b07df0d5b48abf211be819243dc
HunterZwer <117033000+HunterZwer@users.noreply.github.com>
2023-11-15 18:40:45 +0500

    v2 using click argument more deeply

--- realpython_click_tutorials_code.py
+++ realpython_click_tutorials_code.py
@@ -1,24 +1,48 @@
-ls.py v1
+# ls.py v1
+#
+# from pathlib import Path
+#
+# import click
+#
+# @click.command()
+# @click.argument("path")
+# def cli(path):
+#     target_dir = Path(path)
+#     if not target_dir.exists():
+#         click.echo("The target directory doesn't exist")
+#         raise SystemExit(1)
+#
+#     for entry in target_dir.iterdir():
+#         click.echo(f"{entry.name:{len(entry.name) + 5}}", nl=False)
+#
+#     click.echo()
+#
+# if __name__ == "__main__":
+#     cli()
+#
+#
+#
+# ls.py v2
 
 from pathlib import Path
 
 import click
 
 @click.command()
-@click.argument("path")
+@click.argument(
+    "path",
+    type=click.Path(
+        exists=True,
+        file_okay=False,
+        readable=True,
+        path_type=Path,
+    ),
+)
 def cli(path):
-    target_dir = Path(path)
-    if not target_dir.exists():
-        click.echo("The target directory doesn't exist")
-        raise SystemExit(1)
-
-    for entry in target_dir.iterdir():
+    for entry in path.iterdir():
         click.echo(f"{entry.name:{len(entry.name) + 5}}", nl=False)
 
     click.echo()
 
 if __name__ == "__main__":
     cli()
-
-
-
