63c3d2f73fcddcc4e189f345934f0036bc9d7ab0
HunterZwer <117033000+HunterZwer@users.noreply.github.com>
2023-11-15 18:49:13 +0500

    v3 Accepting Variadic Arguments

--- realpython_click_tutorials_code.py
+++ realpython_click_tutorials_code.py
@@ -24,13 +24,40 @@
 #
 # ls.py v2
 
+# from pathlib import Path
+#
+# import click
+#
+# @click.command()
+# @click.argument(
+#     "path",
+#     type=click.Path(
+#         exists=True,
+#         file_okay=False,
+#         readable=True,
+#         path_type=Path,
+#     ),
+# )
+# def cli(path):
+#     for entry in path.iterdir():
+#         click.echo(f"{entry.name:{len(entry.name) + 5}}", nl=False)
+#
+#     click.echo()
+#
+# if __name__ == "__main__":
+#     cli()
+
+
+# ls.py v3
+
 from pathlib import Path
 
 import click
 
 @click.command()
 @click.argument(
-    "path",
+    "paths",
+    nargs=-1,
     type=click.Path(
         exists=True,
         file_okay=False,
@@ -38,11 +65,16 @@ import click
         path_type=Path,
     ),
 )
-def cli(path):
-    for entry in path.iterdir():
-        click.echo(f"{entry.name:{len(entry.name) + 5}}", nl=False)
-
-    click.echo()
+def cli(paths):
+    for i, path in enumerate(paths):
+        if len(paths) > 1:
+            click.echo(f"{path}/:")
+        for entry in path.iterdir():
+            click.echo(f"{entry.name:{len(entry.name) + 5}}", nl=False)
+        if i < len(paths) - 1:
+            click.echo("\n")
+        else:
+            click.echo()
 
 if __name__ == "__main__":
     cli()
