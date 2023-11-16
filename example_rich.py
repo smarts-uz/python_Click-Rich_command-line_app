503c9127b41a13c299005ac734d84affcf20a19b
HunterZwer <117033000+HunterZwer@users.noreply.github.com>
2023-11-16 10:58:44 +0500

    Reading md file with Rich

--- example_rich.py
+++ example_rich.py
@@ -15,55 +15,65 @@
 # 		console.log(f"{task} complete")
 
 
-from rich.align import Align
-from rich.console import Console
-from rich.live import Live
-from rich.table import Table
+# from rich.align import Align
+# from rich.console import Console
+# from rich.live import Live
+# from rich.table import Table
+#
+# TABLE_DATA = [
+# 	[
+# 		"[b white]DSA Course[/]: [i]Beginner[/]",
+# 		"[magenta]$[/]10",
+# 		"[green]Geeks for Geeks[/]",
+# 		"15 hours",
+# 	],
+# 	[
+# 		"[b white]DSA Course[/]: [i]Intermediate[/]",
+# 		"[magenta]$[/]20",
+# 		"[green]Geeks for Geeks[/]",
+# 		"25 hours",
+# 	],
+# 	[
+# 		"[b white]DSA Course[/]: [i]Advanced[/]",
+# 		"[magenta]$[/]30",
+# 		"[green]Geeks for Geeks[/]",
+# 		"30 hours",
+# 	],
+# 	[
+# 		"[b white]Operating System Fundamentals[/]",
+# 		"[magenta]$[/]25",
+# 		"[green]Geeks for Geeks[/]",
+# 		"35 hours",
+# 	],
+# ]
+#
+# console = Console()
+#
+#
+# table = Table(show_footer=False)
+# table_centered = Align.center(table)
+#
+# console.clear()
+#
+# with Live(table_centered, console=console,
+# 		screen=False):
+# 	table.add_column("Course Name", no_wrap=True)
+# 	table.add_column("Price", no_wrap=True)
+# 	table.add_column("Organization", no_wrap=True)
+# 	table.add_column("Duration", no_wrap=True)
+# 	for row in TABLE_DATA:
+# 		table.add_row(*row)
+#
+# 	table_width = console.measure(table).maximum
+#
+# 	table.width =  None
 
-TABLE_DATA = [
-	[
-		"[b white]DSA Course[/]: [i]Beginner[/]",
-		"[magenta]$[/]10",
-		"[green]Geeks for Geeks[/]",
-		"15 hours",
-	],
-	[
-		"[b white]DSA Course[/]: [i]Intermediate[/]",
-		"[magenta]$[/]20",
-		"[green]Geeks for Geeks[/]",
-		"25 hours",
-	],
-	[
-		"[b white]DSA Course[/]: [i]Advanced[/]",
-		"[magenta]$[/]30",
-		"[green]Geeks for Geeks[/]",
-		"30 hours",
-	],
-	[
-		"[b white]Operating System Fundamentals[/]",
-		"[magenta]$[/]25",
-		"[green]Geeks for Geeks[/]",
-		"35 hours",
-	],
-]
+from rich.console import Console
+from rich.markdown import Markdown
 
 console = Console()
+with open("sample.md") as readme:
+	markdown = Markdown(readme.read())
+console.print(markdown)
 
 
-table = Table(show_footer=False)
-table_centered = Align.center(table)
-
-console.clear()
-
-with Live(table_centered, console=console,
-		screen=False):
-	table.add_column("Course Name", no_wrap=True)
-	table.add_column("Price", no_wrap=True)
-	table.add_column("Organization", no_wrap=True)
-	table.add_column("Duration", no_wrap=True)
-	for row in TABLE_DATA:
-		table.add_row(*row)
-
-	table_width = console.measure(table).maximum
-
-	table.width =  None
---
+++ sample.md
@@ -0,0 +1,36 @@
+# Sample markdown file
+Text with some paragraph
+
+2nd paragraph. *Italic*, **bold**, and `monospace`. Itemized lists
+look like:
+
+  * this one
+  * that one
+  * the other one
+
+> Block quotes 
+
+------------
+
+Here's a numbered list:
+
+ 1. first item
+ 2. second item
+ 3. third item
+
+```python
+n = 5
+a = "GFG"
+for i in range(0, 3): 
+  print("Something")
+```
+
+### An h3 header ###
+
+Tables can look like this:
+
+size  material      color
+----  ------------  ------------
+9     leather       brown
+10    hemp canvas   natural
+11    glass         transparent
