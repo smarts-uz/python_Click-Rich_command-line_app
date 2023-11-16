4da031cea7889edce9a994b83588b867c67dfe72
HunterZwer <117033000+HunterZwer@users.noreply.github.com>
2023-11-16 10:52:18 +0500

    An informative tasks display

--- example_rich.py
+++ example_rich.py
@@ -1,3 +1,15 @@
-from rich import print
+# from rich import print
+#
+# print("Hello, [bold green]Geeks[/bold green]!")
 
-print("Hello, [bold green]Geeks[/bold green]!")
+from time import sleep
+from rich.console import Console
+
+console = Console()
+tasks = [f"Task {n}" for n in range(1, 8)]
+
+with console.status("[bold dark_orange]Finishing tasks...") as status:
+	while tasks:
+		task = tasks.pop(0)
+		sleep(1)
+		console.log(f"{task} complete")
